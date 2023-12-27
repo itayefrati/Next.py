c5cd75d2100b92b57fc3702b58c2c4badc22942b
itayefrati <101872520+itayefrati@users.noreply.github.com>
2023-12-27 23:05:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import functools
+# import string
 
 
 def combine_coins(coin, numbers):
@@ -10,6 +11,7 @@ def combine_coins(coin, numbers):
     :return:
     """
     return ', '.join(coin + str(num) for num in numbers)
+
 
 def multi_char(char):
     # multiply a char by 2
@@ -40,9 +42,73 @@ def sum_of_digits(num):
     return functools.reduce(add, str(num))
 
 
+def intersection(list_1, list_2):
+    # returns a list of the common items between two lists
+    return list(set(list_1).intersection(set(list_2)))
+
+
+def is_prime(number):
+    # check if a number is a prime number
+    return (number > 1) and not (True in [number % y == 0 for y in range(2, number)])
+
+
+def is_funny(my_string):
+    # check if a string is only combined from a and h
+    return (len(my_string) > 1) and not (True in [char != 'a' and char != 'h' for char in my_string])
+
+
+def longest_name(file):
+    # this function returns the longest word in a text file
+    with open(file, "r") as file_object:
+        return max(word for word in file_object)
+
+
+def sum_len(file):
+    # this function sums the length of words in a text file
+    with open(file, "r") as f:
+        return sum(len(word) for word in f.read().split())
+
+
+def shortest_name(file):
+    # this function returns the shortest word in a file
+    with open(file, "r") as f:
+        lst = sorted(f.read().split(), key=len)
+        result = (word for word in lst if len(word) == len(lst[0]))
+        return '\n'.join(result)
+
+
+def create_new_file(file):
+    with open(file, "r") as f:
+        with open("name_length.txt", "w") as f2:
+            return f2.write('\n'.join(str(len(name)) for name in f.read().split()))
+
+
 def main():
-    my_list = [x for x in range(6)]
-    print(combine_coins('$', my_list))
+    """password = "sljmai ugrf rfc ambc: lglc dmsp mlc rum"
+    print("".join((chr(ord(char) + 2)) if char in string.ascii_lowercase else char for char in password))"""
+
+    file = r"C:\Users\User\Documents\names.txt"
+    with open(file, "r") as f:
+        names = f.read()
+        # 1: this code returns the longest word in a text file
+        "print(max(word for word in names.split()))"
+
+        # 2: this code sums the length of words in a text file
+        "print(sum(len(word) for word in names.split()))"
+
+        # 3: this code returns the shortest word in a file
+        """lst = sorted(names.split(), key=len)
+        result = (word for word in lst if len(word) == len(lst[0]))
+        print('\n'.join(result))"""
+
+        # 4: this code copies the lengths of words in file to another text file
+        """with open("name_length.txt", "w") as f2:
+            f2.write('\n'.join(str(len(name)) for name in names.split()))"""
+
+        # 5: this code prints all the words from a text file with the given len
+        """num = input("enter a number: ")
+        chosen_names = '\n'.join(name for name in names.split() if len(name) == int(num))
+        print(str(chosen_names))"""
 
 
 if __name__ == '__main__':
